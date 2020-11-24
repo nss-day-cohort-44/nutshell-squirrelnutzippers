16d0bdf19e0f6acb794a166433e91357b5c0fb9d
BryanNilsen <bryannilsen@gmail.com>
2020-11-23 21:43:00 -0600

    removed console.logs

--- src/scripts/weather/WeatherCardRow.js
+++ src/scripts/weather/WeatherCardRow.js
@@ -33,14 +33,13 @@ eventHub.addEventListener("eventWeatherClicked", (e) => {
   // send event zip to get event weather
   getEventWeatherFromZip(event.zip).then(() => {
     const eventWeather = useEventWeather();
-    console.log("eventWeather: ", eventWeather);
     const weatherDay = eventWeather.forecast.daily[dayIndex];
     const eventWeatherHTML = WeatherCardRow(weatherDay);
-    console.log("eventWeatherHTML: ", eventWeatherHTML);
     eventWeatherContainer.innerHTML = eventWeatherHTML;
   });
 });
 
+// close event weather
 eventHub.addEventListener("closeEventWeatherClicked", (event) => {
   const elementId = event.detail.elementId;
   const contentTarget = document.getElementById(elementId);
